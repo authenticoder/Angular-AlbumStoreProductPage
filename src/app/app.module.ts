dde1f519f1084410f58e1bd98f24dd7979b469ea
authenticoder <authenticoderio@gmail.com>
2021-11-16 09:55:43 +0300

    solve s3 challenge 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,7 +20,7 @@ import { ProductDescriptionComponent } from './product-description/product-descr
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
